ff11af916d4220694d8a6668bd4f2b4f4afe91f6
rajmanavadariya <rajmanavadariya2002@gmail.com>
2024-04-09 18:59:52 +0530

    09-04-2024

---
+++ reset-usestate/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Comp from './Comp';
+import Img from './05.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <Comp Image = {Img}/>
+    </div>
+  );
+}
+
+export default App;
