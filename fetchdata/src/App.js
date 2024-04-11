d6d9a0bb17ebb4a288b2336cc546b36e9841aea3
rajmanavadariya <rajmanavadariya2002@gmail.com>
2024-04-11 11:37:16 +0530

    11-04-2024

---
+++ fetchdata/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Fetchdata from './Fetchdata';
+
+function App() {
+  return (
+<>
+
+    <Fetchdata/>
+</>
+  );
+}
+
+export default App;
