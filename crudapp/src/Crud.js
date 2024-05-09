fee1b4cbc6e85f5978976485cc05fabebf8a3893
rajmanavadariya <rajmanavadariya2002@gmail.com>
2024-05-09 18:53:17 +0530

    09-05-2024

---
+++ crudapp/src/Crud.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Crud() {
+    const[inputvalue,setinputvalue] = useState('')
+    const[todos,settodos] = useState([])
+    const[editid,seteditid]=useState(null)
+ 
+    localStorage.setItem("todo",JSON.stringify(todos))
+ 
+    function add(){
+        if(editid !== null){
+            settodos(todos.map(todo=>{
+                if(todo.id === editid){
+                    return{...todo , text:inputvalue}
+                }
+                return todo
+            }))
+
+            seteditid(null)
+            setinputvalue('')
+
+
+
+        }else{
+            let newtodos={
+                id : new Date().getTime(),
+                 text : inputvalue
+             }
+            
+        
+             settodos([...todos,newtodos])
+             setinputvalue('')
+            }
+    }
+    
+
+    function del(id){
+        settodos(todos.filter((todo)=>(
+            todo.id !== id
+        )))
+    }
+
+    function edit(id){
+        const todotoedit = todos.find(todo => todo.id === id)
+        setinputvalue(todotoedit.text)
+        seteditid(id)
+    }
+
+  
+ 
+ 
+ 
+ 
+   return (
+     <div>
+         <h1>TO-DO LIST</h1>
+ 
+         <input type='text' placeholder='Enter your task' value={inputvalue} onChange={(e)=>setinputvalue(e.target.value)}/>
+    
+         <button onClick={add}>Add</button>
+    
+    {
+     todos.map((el,i)=>(
+    
+        <div key={i}>
+          <p key={i}>{el.text}</p>
+          <button onClick={()=>del(el.id)}>Delate</button>
+          <button onClick={()=>edit(el.id)}>Edit</button>
+         </div>
+     ))
+    }
+     </div>
+     
+   )
+ 
+}
+
+export default Crud
