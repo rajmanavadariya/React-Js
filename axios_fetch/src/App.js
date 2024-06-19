c725d9ac57d316c8c38d5e8bb53065af8dd4fb98
rajmanavadariya <rajmanavadariya2002@gmail.com>
2024-06-19 18:51:26 +0530

    19-06-2024

---
+++ axios_fetch/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Postingdata from './Postingdata';
+
+function App() {
+  return (
+    <div className="App">
+         <Postingdata/>
+    </div>
+  );
+}
+
+export default App;
---
+++ axios_fetch/src/Postingdata.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+import React from 'react'
+import { useState } from 'react'
+
+
+function Postingdata() {
+
+
+    const[post,setposts] = useState({
+        title : "",
+        body : ""
+    })
+
+    const handlechange =(event)=>{
+        setposts({...post , [event.target.name] : event.target.value})
+    }
+
+    const handlesubmit=(event)=>{
+        event.preventDefault()
+        axios.post('http://localhost:8000/posts',post)
+        .then((res)=>{
+            console.log(res)
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return (
+    <div>
+        <form onSubmit={handlesubmit}>
+            Title : <input type="text" name='title' onChange={handlechange} value={post.title} />
+            Posts : <input type="text" name='body' onChange={handlechange} value={post.body} />
+            <button>Submit</button>
+        </form>
+    </div>
+  )
+}
+
+export default Postingdata
