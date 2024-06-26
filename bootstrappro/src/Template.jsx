c4c35a89057a46de1c03ad49112d6408589a145a
rajmanavadariya <rajmanavadariya2002@gmail.com>
2024-06-26 17:37:22 +0530

    26-06-2024

---
+++ bootstrappro/src/Template.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './Components/Header'
+import Section1 from './Components/Section1'
+import Section2 from './Components/Section2'
+import Footer from './Components/Footer'
+
+
+
+function Template() {
+  return (
+    <div>        
+   <Header />
+   <Section1 />
+   <Section2 />
+   <Footer />
+    </div>
+  )
+}
+
+export default Template
