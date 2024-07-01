51a4f6789da5e7775c6b0de6a7cbad3d21938483
rajmanavadariya <rajmanavadariya2002@gmail.com>
2024-07-01 17:43:09 +0530

    28-06-2024

---
+++ firebaseauth/src/App.js
@@ -0,0 +1,16 @@
+// App.js
+import React from 'react';
+import SignUp from './Signup';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Firebase Authentication</h1>
+      <SignUp/>
+      
+    </div>
+  );
+};
+
+export default App;
