5a48cf561e0863c187134284b5b1eb09ba0d2659
rajmanavadariya <rajmanavadariya2002@gmail.com>
2024-07-10 16:03:28 +0530

    10-07-2024

---
+++ thunk/src/Counter.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useSelector,useDispatch } from 'react-redux'
+import { increment,decrement } from './store'
+
+function Counter() {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.count);
+
+  return (
+      <div className="container">
+          <h1>Counter: {count}</h1>
+          <button onClick={() => dispatch(increment())}>+</button>
+          <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+  );
+}
+
+export default Counter
